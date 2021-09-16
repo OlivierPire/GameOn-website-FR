d430f69f5c6855957261b430c6162ec6a59202cd
Olivier <olivier.pire@outlook.fr>
2021-09-16 12:10:09 +0200

    Ajout message de validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,77 +13,108 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector(".close");
 const form = document.querySelector("form");
-const locationBtn = document.querySelectorAll(".location-btn");
-const validMessage = document.getElementById("validation");
+const locationBtn = document.querySelectorAll(".location-btn"); // Buttons radio pour l'input "Quelles villes"
+const validMessage = document.getElementById("valid-message"); // Message de validation du formulaire
+const errorLength = document.querySelectorAll(".errorLength"); // Message d'erreur pour nom et prénom si le nombre de caractères n'est pas respecté
+const closeBtn = document.getElementById("close-btn"); // Button fermer, apparaît une fois le formulaire envoyé
 
 //Fonction messages d'erreur
 const errorMessage = (i, style) => {
   const error = document.querySelectorAll(".error");
   error[i].style.display = style;
-}
+} 
 
 // Listen to click event
-close.addEventListener('click', closeModal);
+close.addEventListener('click', closeModal); 
 function closeModal() {
   modalbg.style.display = 'none';
-}
+};
+
+closeBtn.addEventListener("click", closeByBtn); // Button fermer, apparaît une fois le formulaire envoyé
+function closeByBtn() {
+  closeModal();
+};
 
 // Function Control
 const firstControl = () => {
-  if(first.value === "") {
-    errorMessage("0", "inline-block")
-  } else {
+  if(first.value === ""){ // si la value de "first" est vide
+    first.classList.add("input"); // ajoute le style de la classe "input" à "first" (border de input = rouge)
+    errorMessage("0", "inline-block"); // fait appraitre le message d'erreur, "0" correspond à l'objet "first" du tableau "error"
+  } else if (first.value.length < 2 || first.value.length > 20) { // si la value de first est <2 ou >20
+    first.classList.add("input");
+    errorLength[0].style.display = "inline-block";
     errorMessage("0", "none");
+  } else {
+    first.classList.remove("input");
+    errorMessage("0", "none");
+    errorLength[0].style.display = "none";
     return true;
   }
 };
 
 const lastControl = () => {
-  if(last.value === "") {
-    errorMessage("1", "inline-block")
-  } else {
+  if(last.value === "") { 
+    last.classList.add("input");
+    errorMessage("1", "inline-block");
+  } else if (last.value.length < 2 || last.value.length > 20) {
+    last.classList.add("input");
+    errorLength[1].style.display = "inline-block";
     errorMessage("1", "none");
-    return true
+  } else {
+    last.classList.remove("input");
+    errorMessage("1", "none");
+    errorLength[1].style.display = "none";
+    return true;
   }
 };
 
 const emailControl = () => {
   if(email.value === "") {
-    errorMessage("2", "inline-block")
+    email.classList.add("input");
+    errorMessage("2", "inline-block");
   } else {
+    email.classList.remove("input");
     errorMessage("2", "none");
-    return true
+    return true;
   }
 };
+
 const birthdateControl = () => {
   if(birthdate.value === "") {
-    errorMessage("3", "inline-block")
+    birthdate.classList.add("input");
+    errorMessage("3", "inline-block");
   } else {
+    birthdate.classList.remove("input");
     errorMessage("3", "none");
-    return true
+    return true;
   }
 };
+
 const quantityControl = () => {
   if(quantity.value === "") {
-    errorMessage("4", "inline-block")
+    quantity.classList.add("input");
+    errorMessage("4", "inline-block");
   } else {
+    quantity.classList.remove("input");
     errorMessage("4", "none");
-    return true
+    return true;
   }
 };
+
 const locationControl = () => {
-  for(i=0; i<locationBtn.length; i++)
-    if(locationBtn[i].checked) {
+  for(i=0; i<locationBtn.length; i++) // Boucle qui incrémente i à chaque fois jusqu'à ce que i soit égal à "length", i correspond à chaque ville
+    if(locationBtn[i].checked) { // si une ville est "checked"
       errorMessage("5", "none");
       return true;
     } else {
       errorMessage("5", "inline-block");
     };
 };
+
 const checkboxControl = () => {
   if(checkbox1.checked) {
     errorMessage("6", "none");
-    return true
+    return true;
   } else {
     errorMessage("6", "inline-block");
   }
@@ -101,13 +132,13 @@ function validateForm(e) {
   quantityControl();
   locationControl();
   checkboxControl();
-  if(firstControl() && lastControl() && emailControl() && birthdateControl()
-  && quantityControl() && locationControl() && checkboxControl()=== true) {
-    closeModal();
-    validMessage.style.display = "block";
-    
-  }
-}
+  if(firstControl() && lastControl() && emailControl() && birthdateControl() && 
+  quantityControl() && locationControl() && checkboxControl() === true) { // si toutes les functions retournent true 
+    form.style.display = "none";
+    closeBtn.style.display = "inline-block";
+    validMessage.style.display = "inline-block";
+  };
+};
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -115,6 +146,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-}
+};
 
 // close.addEventListener('click', function());
+
